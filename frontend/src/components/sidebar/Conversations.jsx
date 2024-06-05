1ecc4f4b3e3b55347d5484d78071217446bc81fa
isaacmodeyin <modeyinisaac6@gmail.com>
2024-06-05 18:32:59 +0100

    2nd part completed - ui design

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,33 @@
+import Conversation from "./Conversation";
+
+const conversations = () => {
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+    </div>
+  );
+};
+
+export default conversations;
+
+/*  STARTER CODE FOR THIS FILE
+import Conversation from "./Conversation";
+
+const conversations = () => {
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+    </div>
+  );
+};
+*/
